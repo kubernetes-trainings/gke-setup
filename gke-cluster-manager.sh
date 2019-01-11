a99829e76a44224e76c22a5fea9861c403195940
Raghu K <raghuk.vit@gmail.com>
2019-01-11 13:22:17 +0530

    Updating Code - No Comments

---
+++ gke-cluster-manager.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo -e <<EOF
+### This shell script is going to manage the cluster for ease of practice during the trainings.
+### This shell script downaloads a manager into server and can perform the following actions...
+        1. Create Cluster
+        2. Delete Cluster
+        3. Manage Cluster 
+            a. Increase number of nodes
+            b. Delete number of nodes
+EOF
+
+echo Hello 
