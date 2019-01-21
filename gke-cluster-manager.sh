953cf86b6e089aae49741d2fb072aae26a20ab65
Raghu K <raghuk.vit@gmail.com>
2019-01-21 20:04:51 +0530

    --- Auto-Git Commit ---

--- gke-cluster-manager.sh
+++ gke-cluster-manager.sh
@@ -129,7 +129,6 @@ chmod +x /bin/kubernetes-lab-manager
 if [ $2 -eq 0 ]; then 
     echo -e " - \e[32mSUCCESS\e[0m"
 else 
-    echo -e " - \e[31mFAILURE\e[0m"
-    Stop_Check 
+    echo -e " - \e[31mFAILURE\e[0m" 
     exit 1
 fi
